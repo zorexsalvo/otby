dbaebff41be5e7bc28164a8a8d1d61d8f172a3ce
Zorex Salvo <zorexsalvo@gmail.com>
2023-06-24 03:10:58 +0800

    Remove redirects

--- next.config.js
+++ next.config.js
@@ -6,15 +6,6 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "vercel.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/steven-tey/precedent",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
