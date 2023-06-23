21a02da2b3c87e9aa951e6eb5340c47f0945fca7
Zorex Salvo <zorexsalvo@gmail.com>
2023-06-24 02:25:03 +0800

    Add nextjs app

---
+++ otby/otby/app/page.tsx
@@ -0,0 +1,29 @@
+import Balancer from "react-wrap-balancer";
+
+export default async function Home() {
+  return (
+    <>
+      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+        <h1
+          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+        >
+          <Balancer>Online Tambayan Community</Balancer>
+        </h1>
+        <p
+          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
+          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+        >
+          <Balancer>
+          a community filo of gamers and streamers
+          </Balancer>
+        </p>
+        <div
+          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
+          style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
+        >
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ otby/otby/components/layout/navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import useScroll from "@/lib/hooks/use-scroll";
+
+export default function NavBar() {
+  const scrolled = useScroll(50);
+
+  return (
+    <>
+      <div
+        className={`fixed top-0 w-full ${
+          scrolled
+            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+            : "bg-white/0"
+        } z-30 transition-all`}
+      >
+        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between xl:mx-auto">
+          <Link href="/" className="flex items-center font-display text-2xl">
+            <Image
+              src="/logo.png"
+              alt="OTC logo"
+              width="30"
+              height="30"
+              className="mr-2 rounded-sm"
+            ></Image>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
