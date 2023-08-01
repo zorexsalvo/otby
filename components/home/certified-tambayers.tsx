051d8e40428b5485513df82ab29d0611ebcff8be
Zorex Salvo <zorexsalvo@gmail.com>
2023-08-01 08:05:51 +0800

    refactor: Move data to env

--- components/home/certified-tambayers.tsx
+++ components/home/certified-tambayers.tsx
@@ -10,108 +10,7 @@ export default function CertifiedTambayers() {
   const [image, setImage] = useState("");
   const [showModal, setShowModal] = useState(false);
 
-  const certifiedTambayers = [
-    {
-      name: "Sheriff",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609436149383178/sheriff.jpg",
-    },
-    {
-      name: "Ara",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609367006285895/GLADYS.jpg",
-    },
-    {
-      name: "Gnew",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609367262150946/GNEW.jpg",
-    },
-    {
-      name: "Chixy",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609367526387812/JANE.jpg",
-    },
-    {
-      name: "Riri",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609367799009321/JGURBANO.jpg",
-    },
-    {
-      name: "Paulo Plays",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135610426923696191/PAULO.jpg",
-    },
-    {
-      name: "Mushi",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609368608522332/JOEL.jpg",
-    },
-    {
-      name: "Yami",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609368855982090/JOSE.jpg",
-    },
-    {
-      name: "Kanchoy",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609369141199018/KANCHOY.jpg",
-    },
-    {
-      name: "Neku",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609369409630339/NEKU.jpg",
-    },
-    {
-      name: "Phia",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609436409434223/SOPHIA.jpg",
-    },
-    {
-      name: "Pitchy",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609435411185664/PITCHY.jpg",
-    },
-    {
-      name: "Rizza",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609435683819591/RIZZA.jpg",
-    },
-    {
-      name: "Ryan",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609435918708886/RYANM.jpg",
-    },
-    {
-      name: "Jojo",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609437453824141/ZOREX.jpg",
-    },
-    {
-      name: "Vicente",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609436627546133/VICENTE.jpg",
-    },
-    {
-      name: "Kain Mo",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609436929523834/WILMER.jpg",
-    },
-    {
-      name: "Jenella",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609437176991824/ZADGR.jpg",
-    },
-    {
-      name: "Bubbles",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609368356851783/JOANNA_MAE.jpg",
-    },
-    {
-      name: "Jiji",
-      image:
-        "https://cdn.discordapp.com/attachments/1135609067595894835/1135609368050679838/JIJI.jpg",
-    },
-  ];
+  const certifiedTambayers = JSON.parse(process.env.NEXT_PUBLIC_DATA);
 
   return (
     <div className="m:px-0 z-10 w-full max-w-4xl pt-24 xl:px-20">
@@ -145,7 +44,10 @@ export default function CertifiedTambayers() {
       </div>
       <Modal showModal={showModal} setShowModal={setShowModal}>
         <div className="w-full overflow-hidden md:max-w-md md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
-          <div className="flex flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:px-16">
+          <div
+            className="' flex
+          flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:px-16"
+          >
             <a href="#"></a>
             <Image
               src={image}
