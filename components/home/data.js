7ce4d79ba971a026c9244d30d6b9388dd060eb9d
Zorex <zorexsalvo@gmail.com>
2024-03-28 20:03:56 +0800

    fix: Make image link static assets

--- components/home/data.js
+++ components/home/data.js
@@ -2,108 +2,121 @@ const data = [
   {
     name: "Sheriff",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609436149383178/sheriff.jpg",
+      "/avatars/sheriff.jpg",
   },
   {
     name: "Ara",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609367006285895/GLADYS.jpg",
+      "/avatars/gladys.jpg",
   },
   {
     name: "Gnew",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609367262150946/GNEW.jpg",
+      "/avatars/weng.jpg",
   },
   {
     name: "Chixy",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609367526387812/JANE.jpg",
+      "/avatars/jane.jpg",
   },
   {
     name: "Riri",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609367799009321/JGURBANO.jpg",
+      "/avatars/jgurbano.jpg",
   },
   {
     name: "Paulo Plays",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135610426923696191/PAULO.jpg",
+      "/avatars/paulo.jpg",
   },
   {
     name: "Mushi",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609368608522332/JOEL.jpg",
+      "/avatars/joel.jpg",
   },
   {
     name: "Yami",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609368855982090/JOSE.jpg",
+      "/avatars/jose.jpg",
   },
   {
     name: "Kanchoy",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609369141199018/KANCHOY.jpg",
+      "/avatars/kanchoy.jpg",
   },
   {
     name: "Neku",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609369409630339/NEKU.jpg",
+      "/avatars/neku.jpg",
   },
   {
     name: "Phia",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609436409434223/SOPHIA.jpg",
+      "/avatars/sophia.jpg",
   },
   {
     name: "Pitchy",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609435411185664/PITCHY.jpg",
+      "/avatars/pitchy.jpg",
   },
   {
     name: "Rizza",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609435683819591/RIZZA.jpg",
+      "/avatars/rizza.jpg",
   },
   {
     name: "Ryan",
-    image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609435918708886/RYANM.jpg",
+    image: "/avatars/ryanm.jpg",
   },
   {
     name: "Jojo",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609437453824141/ZOREX.jpg",
+      "/avatars/zorex.jpg",
   },
   {
     name: "Vicente",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609436627546133/VICENTE.jpg",
+      "/avatars/vicente.jpg",
   },
   {
     name: "Kain Mo",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609436929523834/WILMER.jpg",
+      "/avatars/wilmer.jpg",
   },
   {
     name: "Jenella",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609437176991824/ZADGR.jpg",
+      "/avatars/zadge.jpg",
   },
   {
     name: "Bubbles",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609368356851783/JOANNA_MAE.jpg",
+      "/avatars/joanna_mae.jpg",
   },
   {
     name: "Jiji",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135609368050679838/JIJI.jpg",
+      "/avatars/jiji.jpg",
   },
   {
     name: "Marielle",
     image:
-      "https://cdn.discordapp.com/attachments/1135609067595894835/1135724468220727386/MARIELLE.jpg",
+      "/avatars/marielle.jpg",
   },
+  {
+    name: "Rotugal",
+    image:
+      "/avatars/rotugal.jpg",
+  },
+  {
+    name: "Daam",
+    image:
+      "/avatars/dam_liit.jpg",
+  },
+  {
+    name: "Maxene",
+      image: "/avatars/maxene.jpg",
+  }
 ];
 
 export default data;
